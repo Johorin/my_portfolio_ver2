13fc9a0437ae782bb335e071017a9df90f9063dd
Johorin <hoyuak2145@docomo.ne.jp>
2024-10-27 19:02:05 +0900

    path調整とTOPページ調整

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+const Header = () => {
+    return (
+        <header>
+            <h1>HOJO YUYA</h1>
+            <ul>
+                <li>TOP</li>
+                <li>TESTLIST</li>
+                <li>SKILLS</li>
+                <li>CONTACT</li>
+            </ul>
+        </header>
+    )
+}
+
+export default Header
